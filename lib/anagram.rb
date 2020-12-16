1a6d20c85ab8e5b4cef55ccdb7633f795bf04ff5
Victoria Thorsen <victoriathorsenn@gmail.com>
2020-12-15 16:47:01 -0800

    commit tree e1c3ab6b77cabd7b499e49d58373963d04d864c6

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(string)
+        string.select do |words|
+            words.split("").sort == word.split("").sort
+        end
+    end
+
+
+
+end
+
+
+
+# if word.split("") == 0
+    puts
+
